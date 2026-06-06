255a9862c3d700fda4025044c3db7f9762e60947
Parth Rajurkar <pvrajurkar1@gmail.com>
2026-06-06 13:56:50 +0530

    euclidean distance method added

--- coordinate class.py
+++ coordinate class.py
@@ -5,8 +5,11 @@ class Point:
         
     def __str__(self):
         return '<{},{}>'.format(self.x_cod,self.y_cod)
+    
+    def euclidean_distance(self,other):
+        return ((self.x_cod - other.x_cod)**2 + (self.y_cod - other.y_cod)**2)**0.5
         
 p1=Point(0,0)
 p2=Point(-2,-1)
-print(p1)
-print(p2)
+
+print(p1.euclidean_distance(p2))
