d3c4991dcf853af46e74ef2190502c88f2687465
Parth Rajurkar <pvrajurkar1@gmail.com>
2026-06-09 14:03:54 +0530

    Comments added to Phone Inheritance 2.py

--- Phone Inheritance 2.py
+++ Phone Inheritance 2.py
@@ -1,14 +1,20 @@
+#Parent Class
 class Phone:
+    #this constructor will not be executed as Child Class has an Constructor
     def __init__(self,price,brand,camera):
         print("Inside Phone Constructor")
         self.__price=price
         self.brand=brand
         self.camera=camera
-        
+
+#Child Class        
 class SmartPhone(Phone):
+    #This Constructor will be Executed
     def __init__(self,os,ram):
         print("Inside SmartPhone Constructor")
         self.os=os
         self.ram=ram        
         
-s=SmartPhone('Android',8)        
+s=SmartPhone('Android',8)
+print(s.os)
+print(s.ram)        
