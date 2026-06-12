d48ccb53324aaa7db1bb3397d33c530452394dd7
Parth Rajurkar <pvrajurkar1@gmail.com>
2026-06-12 21:36:40 +0530

    dictionary data type saved in json file using serialization

--- serialization json.py
+++ serialization json.py
@@ -4,3 +4,9 @@ L=[1,2,3,4,5]
 
 with open('demo.json','w') as f:
     json.dump(L,f)
+    
+    
+d={'name':'Parth','age':21,'gender':'male'}
+
+with open('demo1.json','w') as f:
+    json.dump(d,f,indent=4)
