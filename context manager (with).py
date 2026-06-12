0dd63c4f55179b1a0940c552eaf5b539f5837611
Parth Rajurkar <pvrajurkar1@gmail.com>
2026-06-12 13:24:23 +0530

    'with' used to read file

--- context manager (with).py
+++ context manager (with).py
@@ -1,3 +1,6 @@
 #In this,File gets close automatically
 with open('Samplefile2.txt','w') as f:
     f.write("Hello,You are using context manager 'with' ")
+    
+with open('Samplefile2.txt','r') as f:
+    print(f.read())
