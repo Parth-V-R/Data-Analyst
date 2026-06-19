f73957c7cacfd7ef4a4bbe162dd7db1f6cb1daba
Parth Rajurkar <pvrajurkar1@gmail.com>
2026-06-19 19:18:34 +0530

    zeros fn used to make default values 0 in array

---
+++ Numpy/zeros and ones function in numpy.py
@@ -0,0 +1,12 @@
+# zeros fn is used when all default values should be zero
+#syntax- numpy.zeros(shape),ex- shape 3 (0,0,0),shape 2 ((0,0))
+
+import numpy as np
+arr_zero=np.zeros(3)
+print(arr_zero)
+
+arr_zero1=np.zeros((2,2))
+print(arr_zero1)
+
+arr_zero2=np.zeros((2,3,4))
+print(arr_zero2)
