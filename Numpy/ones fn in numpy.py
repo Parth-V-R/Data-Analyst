61a9eed54fddcc77cd0785d4ea067ee33def16f0
Parth Rajurkar <pvrajurkar1@gmail.com>
2026-06-19 19:28:16 +0530

    ones fn used to create array with default value 1

---
+++ Numpy/ones fn in numpy.py
@@ -0,0 +1,9 @@
+# ones fn is used when all default values should be one
+#syntax- numpy.ones(shape),ex- shape 3(1,1,1)
+
+import numpy as np
+arr_1=np.ones(3)
+print(arr_1)
+
+arr_2=np.ones((2,3))
+print(arr_2)
