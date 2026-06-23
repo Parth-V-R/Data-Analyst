c05ff550dba7072694bc81b329fb0f36035aea94
Parth Rajurkar <pvrajurkar1@gmail.com>
2026-06-23 19:50:09 +0530

    made changes to app.py

--- Pandas/app.py
+++ Pandas/app.py
@@ -2,7 +2,10 @@ import pandas as pd
 
 #read data from CSV file into a dataframe(2D labelled aray)
  
-df=pd.read_csv("D:\Asus\OneDrive\Desktop\Data-Analyst\Pandas\SampleSuperstore.xlsx - Orders.csv",encoding="latin1")
+#df=pd.read_csv("D:\Asus\OneDrive\Desktop\Data-Analyst\Pandas\SampleSuperstore.csv")
+#print(df)
 
-print(df)
+
+#df=pd.read_json("D:\Asus\OneDrive\Desktop\Data-Analyst\Pandas\sample_Data.json")
+#print(df)
 
