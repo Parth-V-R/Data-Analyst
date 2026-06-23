fbf14fb19403fa33bfbf16ceeff35b447f39600d
Parth Rajurkar <pvrajurkar1@gmail.com>
2026-06-23 19:49:31 +0530

    sample_Data json file added

---
+++ Pandas/app.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+#read data from CSV file into a dataframe(2D labelled aray)
+ 
+df=pd.read_csv("D:\Asus\OneDrive\Desktop\Data-Analyst\Pandas\SampleSuperstore.xlsx - Orders.csv",encoding="latin1")
+
+print(df)
+
