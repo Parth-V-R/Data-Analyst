88959cb56258eaffe67821b48bc50a2629a3c39a
Parth Rajurkar <pvrajurkar1@gmail.com>
2026-06-26 21:34:42 +0530

    accesses a single column from DataFrame

---
+++ Pandas/example1.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+data={
+    "Name":["Ram","Shyam","Ganshyam","Dhanshyam","Amit","Jagdish","Raj","Simran"],
+    "Age":[28,34,22,30,29,40,25,32],
+    "Salary":[50000,60000,45000,52000,49000,70000,48000,58000],
+    "Performance Score":[85,90,78,92,88,95,80,89]
+}
+
+df=pd.DataFrame(data)
+
+#Displaying DF
+print("Sample Dataframe")
+print(df)
+
+#Displaying Single Column-"Names",single column returns series
+print("Names")
+#name=df["Name"]
+#print(name)
+
+print(df["Name"])
