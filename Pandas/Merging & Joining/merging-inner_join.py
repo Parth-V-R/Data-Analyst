6833177dfc0d2a4d28ffce5b9d9a3de87230f8ac
Parth Rajurkar <pvrajurkar1@gmail.com>
2026-07-01 16:20:47 +0530

    merge used to inner join,to merge only values of same on ""

---
+++ Pandas/Merging & Joining/merging-inner_join.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+df_customers=pd.DataFrame({
+    "CustomerID":[1,2,3],
+    "Name":['Ramesh','Suresh','Kalpesh']
+})
+
+df_orders=pd.DataFrame({
+    "CustomerID":[1,2,4],
+    "OrderAmount":[250,450,350]
+})
+
+#in inner join,only matching values of on="" are merged
+
+df_merged=pd.merge(df_customers,df_orders,on="CustomerID",how='inner')
+print(df_merged)
