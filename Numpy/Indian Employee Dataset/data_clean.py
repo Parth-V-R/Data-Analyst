5172b303e7d24185c2c807b722740d34215078b0
Parth Rajurkar <pvrajurkar1@gmail.com>
2026-07-02 10:29:44 +0530

    missing values in each column are checked

--- Numpy/Indian Employee Dataset/data_clean.py
+++ Numpy/Indian Employee Dataset/data_clean.py
@@ -6,3 +6,11 @@ import pandas as pd
 df=pd.read_csv("Numpy/Indian Employee Dataset/Indian_Employee_Data.csv")
 print(df.head())
 
+#checking the missing values
+
+print("Missing Values in each Column")
+print(df.isnull().sum())
+"""df.isnull().sum(),returns null values in each column"""
+
+
+
